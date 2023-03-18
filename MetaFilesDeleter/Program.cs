16ff5e6a40861621007fb6555972881778d28995
Erick <erick.dev.miller@gmail.com>
2023-03-18 12:59:27 +0500

    first commit, removing meta files

---
+++ MetaFilesDeleter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MetaFilesDeleter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> folders = new List<string>();
+            List<string> files = new List<string>();
+
+            string metaFilesFormat = ".meta";
+            string path = "";
+            GetPath(ref path);
+
+            folders = Directory.GetDirectories(path, "*", SearchOption.AllDirectories).ToList();
+
+            for (int i = 0; i < folders.Count; i++)
+            {
+                foreach (var file in Directory.GetFiles(folders[i]))
+                {
+                    if (file.EndsWith(metaFilesFormat))
+                    {
+                        File.Delete(file);
+                    }
+                }
+            }
+
+            Console.WriteLine("Мета файлы успешно удалены \n");
+        }
+
+        static void GetPath(ref string path)
+        {
+            Console.WriteLine("Введите путь до папки");
+            path = Console.ReadLine();
+            
+            if (!Directory.Exists(path))
+            {
+                GetPath(ref path);
+            }
+        }
+    }
+}
